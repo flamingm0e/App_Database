cf448e8458fbd2e409e33b9f9572866ebceb10c6
sbesh91 <s.beshensky@gmail.com>
2015-03-31 14:26:15 -0500

    fix filters by making a custom filter function.

--- public/js/controllers/AppCtrl.js
+++ public/js/controllers/AppCtrl.js
@@ -10,7 +10,7 @@ angular.module('AppCtrl', [])
         $scope.updateApp = function (item) {
             appsData.update({category: getPath}, item);
         };
-        $scope.global.fil.complete = false;
+
     })
     .controller('MainController', function ($rootScope, $scope, appConfig, getPath, getCats, $window, $modal) {
         $scope.projectName = appConfig.name;
@@ -18,20 +18,46 @@ angular.module('AppCtrl', [])
             $window.location.reload();
         };
         var location = getPath;
-        var location = location.replace('-', ' & ');
-        var location = location.replace('_', ' ');
+        location = location.replace('-', ' & ');
+        location = location.replace('_', ' ');
         $scope.appHeader = 'Applications in '+location+' category';
         $rootScope.global = {
             search: '',
             categories: getCats.query(),
-            fil: { }
+            fil: { },
+            filterMethod: function(){
+              return function(item)
+              {
+                var filters = $rootScope.global.fil;
+                if((typeof filters.complete === 'undefined' || filters.complete === false) &&
+                  (typeof filters.stage === 'undefined' || filters.stage === false) &&
+                  (typeof filters.fix === 'undefined' || filters.fix === false))
+                  return true;
+
+                var filterArray = [];//0 == complete, 1 == stage, 2 == fix
+                filterArray.push(
+                  (typeof filters.complete === 'undefined' || filters.complete === false ? false : true),
+                  (typeof filters.stage === 'undefined' || filters.stage === false ? false : true),
+                  (typeof filters.fix  === 'undefined' || filters.fix === false? false : true));
+
+                var itemStatus = [];
+                itemStatus.push(item.complete,item.stage,item.fix);
+
+                for(var i = 0; i < filterArray.length; i++){
+                  if(itemStatus[i] === false && filterArray[i] === true){
+                    return false;
+                  }
+                }
+                return true;
+              };
+            }
         };
         $scope.openAppForm = function (size) {
             var modalInstance = $modal.open({
                 templateUrl:'views/applicationform.html',
                 controller: 'AddController',
                 size : size
-            })
+            });
         };
         $scope.showAll = function() {
             $scope.global.fil = {};
@@ -58,5 +84,5 @@ angular.module('AppCtrl', [])
         $scope.reset();
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
-        }
+        };
     });
