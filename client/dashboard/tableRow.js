0b4e6e658e3693159bf0f9f6586f2127c2cf954d
m@ <matt@jmf-tech.com>
2015-05-26 00:12:15 -0500

    setting up db update

--- client/dashboard/tableRow.js
+++ client/dashboard/tableRow.js
@@ -16,8 +16,18 @@
 Template.tableRow.events({
   "click .toggle-checked": function () {
     var path = Iron.controller().params.category.capFirst();
+    var status = {
+      complete: $('input[name=_id]').prop('checked') ? true : false,
+      stage: $('input[name=stage]:checked').val(),
+      fix: $('input[name=fix]:checked').val()
+    };
+    console.log(status);
     // Set the checked property to the opposite of its current value
-    console.log(path +" "+ this._id);
-    //DB[path].update(this._id, {$set: {checked: ! this.checked}});
+    //DB[path].update(this._id, {$set: {complete: ! this.complete}});
+    // DB[path].update(this._id, {$set: status}, function(error) {
+    //   if (error) {
+    //     alert(error.reason);
+    //   }
+    // });
   },
 });
